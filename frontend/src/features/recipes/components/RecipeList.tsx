4691f37bb26e9eea9118a218d3b44e32a200e5c0
Érico Moura <erico.moura@sap.com>
2026-01-13 15:05:57 -0300

    Added in stock filter

--- frontend/src/features/recipes/components/RecipeList.tsx
+++ frontend/src/features/recipes/components/RecipeList.tsx
@@ -1,7 +1,8 @@
 import { useState } from 'react';
-import type { Ingredient, Recipe, RecipeItem } from '../../../types';
+import type { Ingredient, Recipe, RecipeItem, RecipeListFilters } from '../../../types';
 import styles from './RecipeList.module.css';
 import { RecipeListItem } from './RecipeListItem';
+import utils from '../../../styles/utilities.module.css';
 
 interface RecipeListProps {
   items: Recipe[];
@@ -22,33 +23,60 @@ export function RecipeList({ items, ingredients, onDelete, onEdit }: RecipeListP
 
     return ascending ? sorted : sorted.reverse();
   }
+  function filterRecipes(recipes: Recipe[], filters: RecipeListFilters): Recipe[] {
+    let filtered = recipes
+    .filter(recipe => {
+      if (filters.inStock) {
+        return recipe.items.every(item => {
+          const ingredient = ingredients.find(ing => ing.id === item.ingredientId);
+          return ingredient ? ingredient.amount >= item.amount : false;
+        })
+      }
+      return true;
+    })
+
+    return filtered;
+  }
 
 
   const [sortCriteria, setSortCriteria] = useState<string>('name');
   const [sortAscending, setSortAscending] = useState<boolean>(true);
-  const sortedRecipes = sortRecipes(items, sortCriteria, sortAscending);
+  const [filters, setFilters] = useState<RecipeListFilters>({ inStock: false });
+  const filteredSortedRecipes = sortRecipes(filterRecipes(items, filters), sortCriteria, sortAscending);
   
   
 
   return (
     <section className={styles.section}>
-      <h4 id="saved-recipes-heading">Saved Recipes</h4>
-      <div className={styles.sortMenu}>
-          <label>Sort by:</label>
-          <select
-            value={sortCriteria}
-            onChange={e => setSortCriteria(e.target.value)}>
-              <option value="name">Name</option>
-              <option value="ingredients">Ingredients</option>
-          </select>
-          <button onClick={() => setSortAscending(!sortAscending)}>{sortAscending ? '↑' : '↓'}</button>
+      <h4>Saved Recipes</h4>
+      <div className={styles.filterSortRow}>
+        <div className={styles.sortMenu}>
+            <label>Sort by:</label>
+            <select
+              value={sortCriteria}
+              onChange={e => setSortCriteria(e.target.value)}>
+                <option value="name">Name</option>
+                <option value="ingredients">Ingredients</option>
+            </select>
+            <button onClick={() => setSortAscending(!sortAscending)}>{sortAscending ? '↑' : '↓'}</button>
+        </div>
+
+        <div className={utils.vDivider} />
+
+        <div>
+          <label>Filter by: </label>
+          <label><input 
+            type='checkbox'
+            checked={filters.inStock}
+            onChange={e => setFilters(prev => ({...prev, inStock: e.target.checked}))} />In stock</label>
+        </div>
       </div>
       
-      {sortedRecipes.length === 0 ? (
+      {filteredSortedRecipes.length === 0 ? (
         <p className={styles.empty}>No recipes available.</p>
       ) : (
         <ul className={styles.list}>
-          {sortedRecipes.map(recipe => {
+          {filteredSortedRecipes.map(recipe => {
             return (
               <RecipeListItem
                 key={recipe.id}
